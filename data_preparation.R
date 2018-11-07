8c0e95a3d419736b43fa2108e94bd2e20a0c31f3
Cameron Kay <cameronstuartkay@gmail.com>
2018-11-07 09:56:07 -0800

    Changed pound/octothorp/hashtags to accord with a more conventional style.

--- data_preparation.R
+++ data_preparation.R
@@ -8,7 +8,7 @@
 ###set up
 ###############################
 
-###load required packages
+#load required packages
 library(tidyverse)
 library(lme4)
 library(lmerTest)
@@ -16,16 +16,16 @@ library(here)
 library(rio)
 library(lubridate)
 
-###create function to find column numbers given column names
+#create function to find column numbers given column names
 .rcol <- function(column.name.as.string = NULL, data = df) {
   grep(column.name.as.string,colnames(data))
 }
 
-###import data
+#import data
 df <- import(here::here("data", "dataSPSS.sav"), setclass = "tibble") %>%
   janitor::clean_names()
 
-###characterize all columns except for age and books1 (the spss labels were causing the numeric values to be recorded as NAs for those columns)
+#characterize all columns except for age and books1 (the spss labels were causing the numeric values to be recorded as NAs for those columns)
 df[, -c(.rcol("age"), 
         .rcol("books1"))] <- characterize(df[, -c(.rcol("age"), 
                                                   .rcol("books1"))])
@@ -34,7 +34,7 @@ df[, -c(.rcol("age"),
 ###data tidying
 ###############################
 
-###select only variables of interest
+#select only variables of interest
 df <- df %>%
   select(-comp, 
          -state:-qs1,
@@ -47,34 +47,34 @@ df <- df %>%
          -birth_hisp,
          -partyln:-cellweight)
   
-###parse date for interview date column
+#parse date for interview date column
 df <- df %>%
   mutate(int_date = ymd(int_date))
 
-###remove participants who do not occasionally use the internet or email
+#remove participants who do not occasionally use the internet or email
 df <- df %>% 
   filter(eminuse == "Yes") %>%
   select(-eminuse)
 
-###move never-use social media sites from the columns starting web to the frequency columns
+#move never-use social media sites from the columns starting web to the frequency columns
 
-#gather names for never-use columns and for frequency-use columns
+##gather names for never-use columns and for frequency-use columns
 never_use <- grep("^web1", colnames(df))
 freq_use <- grep("^sns2", colnames(df))
 
-#change "no, do not do this" in the web columns to "Rarely if ever" in the sns columns
+##change "no, do not do this" in the web columns to "Rarely if ever" in the sns columns
 for (i in 1:length(never_use)) {
   df[which(df[ , never_use[i]] == "No, do not do this"), freq_use[i]] <- "Rarely if ever"
 }
 
-#drop web columns
+##drop web columns
 df <- df %>%
   select(-starts_with("web1"))
 
-#drop unneeded values
+##drop unneeded values
 rm(freq_use, never_use)
 
-###tidy the data according to the SNS columns
+#tidy the data according to the SNS columns
 df <- df %>%
   gather(key = "sns", value = "freq_usage", starts_with("sns2")) %>%
   mutate(sns = recode(sns, sns2a = "Twitter",
@@ -83,7 +83,7 @@ df <- df %>%
                            sns2d = "Snapchat",
                            sns2e = "YouTube"))
 
-###rename poorly named columns for sanity
+#rename poorly named columns for sanity
 df <- df %>%
   rename(id = respid,
          date = int_date,
